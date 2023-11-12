55515f8630df5ccabac545c04032f3800f936b0e
Daniil Shcherbakov <djdaniil46@gmail.com>
2023-11-12 03:25:34 +0100

    set up

---
+++ backend/src/main/java/com/coyjiv/springbankadminpanel/SpringBankAdminPanelApplication.java
@@ -0,0 +1,36 @@
+package com.coyjiv.springbankadminpanel;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@SpringBootApplication
+public class SpringBankAdminPanelApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankAdminPanelApplication.class, args);
+	}
+
+
+	@Override public void run(ApplicationArguments args) throws Exception {
+		System.out.println("http://localhost:9000/swagger-ui/index.html \n");
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("BAP API")
+						.description("Bank admin platform")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+						.description("SpringShop Wiki Documentation")
+						.contact(new Contact().email("djdanil46@gmail.com").url("https://daniilshcherbakov.vercel.app/")))
+				;
+	}
+}
---
+++ backend/src/test/java/com/coyjiv/springbankadminpanel/SpringBankAdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coyjiv.springbankadminpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankAdminPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
